ec31011b19515a822f91b1c3d0f09b24c7be286d
andruby <andrew@bedesign.be>
2012-07-28 21:15:24 -0700

    Use Tilt and erb templates to cleanup main app

--- whoisbot.rb
+++ whoisbot.rb
@@ -1,5 +1,6 @@
 require 'bundler'
 Bundler.require
+require 'erb'
 
 problem_tlds = []
 TLDS = Whois::Server.definitions[:tld].map(&:first).reject { |v| v.length > 5 } - problem_tlds
@@ -10,7 +11,7 @@ class SuperWho
   def color_for_domain(base, tld, env)
     domain = base + tld
     color = Whois.whois(domain).available? ? 'green' : 'red' rescue 'orange'
-    env.css_for(tld, color)
+    env.template(:color, tld: tld, color: color)
     true
   end
 end
@@ -22,20 +23,19 @@ class EnvWrapper
     @env = env
   end
 
-  def css_for(tld, color)
-    stream_send <<-EOS
-  <style type='text/css'>
-    .#{tld.gsub('.','_')} { background-color: #{color}; color: white; font-weight: bold}
-  </style>
-  EOS
-  end
-
   def stream_send(txt)
     @env.stream_send(txt)
   end
 
   def stream_close
     @env.stream_close
+  end
+
+  def template(*filenames)
+    locals = filenames.pop if filenames.last.is_a?(Hash)
+    filenames.each do |filename|
+      stream_send Tilt.new("templates/#{filename}.html.erb").render(Object.new, locals || {})
+    end
   end
 end
 
@@ -49,25 +49,26 @@ class Whoisbot < Goliath::API
   def response(env)
     e = EnvWrapper.new(env)
     if (base_domain = params['query'])
-      [200, {'Content-Type' => 'text/html'}, Goliath::Response::STREAMING]
-      e.stream_send "<style type='text/css'>.box { float:left; width: #{(base_domain.length * 8) + 50}px; border: 1px solid black; padding: 3px} </style>"
-      e.stream_send "<h3>Checking #{TLDS.count} tlds for base domain #{base_domain}</h3>"
-      e.stream_send "<div>"
-      TLDS.each { |tld| e.stream_send "<div class='box #{tld.gsub('.','_')}' style=''>#{base_domain + tld}</div>" }
-      e.stream_send "</div>"
-      e.stream_send "<div style='clear: both'>by <a href='http://twitter.com/andruby'>@andruby</a></div>"
-      pool = SuperWho.pool(size: 50)
-      futures = TLDS.map { |tld| pool.future(:color_for_domain, base_domain, tld, e) }
-      EM.defer do
-        futures.each { |f| f.value }
-        e.stream_close
-      end
+      query(e, base_domain)
     else
-      response = ""
-      response << "<h4>Base Domain</h4>"
-      response << "<form method='get'><input type='text' name='query'><input type='submit'></from>"
-      response << "<p>by <a href='http://twitter.com/andruby'>@andruby</a></p>"
-      [200, {'Content-Type' => 'text/html'}, response]
+      root(e)
+    end
+    [200, {'Content-Type' => 'text/html'}, Goliath::Response::STREAMING]
+  end
+
+  def root(env)
+    env.template(:head, :root, :credits, :foot)
+    env.stream_close
+  end
+
+  def query(env, base_domain)
+    env.template(:head, :boxes, :credits, base_domain: base_domain, tlds: TLDS)
+    EM.defer do
+      pool = SuperWho.pool(size: 50)
+      futures = TLDS.map { |tld| pool.future(:color_for_domain, base_domain, tld, env) }
+      futures.each(&:value)
+      env.template(:foot)
+      env.stream_close
     end
   end
 end
