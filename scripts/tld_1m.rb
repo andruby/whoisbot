251839805aac5987d45afcfc3bf7bc221538f0b3
Andrew Fecheyr <andrew@bedesign.be>
2015-06-05 00:22:57 +0200

    script to count tlds of top 1M sites

---
+++ scripts/tld_1m.rb
@@ -0,0 +1,19 @@
+require 'whois'
+require 'csv'
+
+sorted_tlds = Whois::Server.definitions[:tld].map(&:first).sort_by { |x| x.length * -1 }
+top_tlds = Hash.new(0)
+
+i = 0
+CSV.foreach("data/top-1m.csv") do |row|
+  domain = row[1]
+  tld = sorted_tlds.detect { |tld| domain[-(tld.length)..-1] == tld }
+  top_tlds[tld] += 1
+  p i if (i+=1) % 20000 == 0
+end
+
+CSV.open("research/tld_top_1m.csv", "wb") do |csv|
+  top_tlds.each do |tld, toppers|
+    csv << [tld, toppers]
+  end
+end
